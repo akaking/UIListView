df91cbb5a354f72eb5c415d0b7daab84ce6641d1
yongbin <yongbin.ios@gmail.com>
2012-11-12 11:15:17 +0800

    Initial Commit

---
+++ UIListView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIListView
+//
+//  Created by SeeKool on 11/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
